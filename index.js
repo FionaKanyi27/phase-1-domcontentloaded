0357eea1cdc8cb63e3ca940f7a79a672d1bf0605
FionaKanyi <fionakanyi0@gmail.com>
2024-10-21 22:58:15 +0300

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Initially set the text content of the paragraph
+document.querySelector('p').textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
+
+// Event listener for DOMContentLoaded to change the text
+document.addEventListener("DOMContentLoaded", function() {
+  const paragraph = document.getElementById("text");
+  paragraph.textContent = "This is really cool!";
+});
